a380f5041da271a7950b94d919664d2b7976b6a5
romkhonglun <nguyenhuucongzz01@gmail.com>
2025-12-01 15:36:54 +0700

    create exp v1xxx

---
+++ src/exp/v1xxx_training/run_pl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+
+EXP_DIR=$(cd $(dirname $0)/..; pwd)
+DEVICE=0
+MODEL=$EXP_DIR/v1xxx_training/v1184_111_PL_bert_L4_256d.py
+uv run python $MODEL train common.fold=0 trainer.devices=[${DEVICE}] common.overwrite=True
+uv run python $MODEL train common.fold=2 trainer.devices=[${DEVICE}] common.overwrite=True
+uv run python $MODEL predict --split validation common.fold=0 trainer.devices=[${DEVICE}]
+uv run python $MODEL predict --split test common.fold=2 trainer.devices=[${DEVICE}]
